2b8fd2b68deb1e22207ae35c2a5953b0109acf64
habibaadam <habiepalmer@gmail.com>
2023-12-06 12:58:40 -0800

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,63 +1,50 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 1-pack_web_static.py) that distributes an
-archive to your web servers, using the function do_deploy:"""
+# A script that transfers an archive to a web server.
 
-from fabric.api import *
-import os
-from datetime import datetime
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-env.user = 'ubuntu'
 env.hosts = ['54.205.99.166', '100.26.122.165']
 
 
-def do_pack():
-    """ Generates a .tgz archive from the contents of the
-    web_static folder
+def do_deploy(archive_path):
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    now = datetime.today()
-    file = f"versions/web_static_{now.year}{now.month:02d}{now.day:02d}{now.hour:02d}" \
-           f"{now.minute:02d}{now.second:02d}.tgz"
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    print(f"Packing web_static to {file}")
-    if not os.path.exists("versions/"):
-        local("mkdir versions/")
-
-    res = local(f"tar -cvzf {file} web_static/")
-    if res.failed:
-        return None
-    else:
-        archive_size = os.path.getsize(file)
-        print(f"web_static packed: {file} -> {archive_size} Bytes")
-        return file
-
-def do_deploy(archive_p):
-    """Distributes an archive to the web servers"""
-    if not os.path.exists(archive_p):
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-
-    path = archive_p.split('/')
-    archive_file = path[1]
-    splitted_arhive = archive_file.split('.')
-
-    server_archive_path = f'/tmp/{archive_file}'
-    xarch_path = f'/data/web_static/releases/{splitted_arhive[0]}'
-
-    if put(archive_p, '/tmp/').failed:
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    if run(f'mkdir -p {xarch_path}').failed:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    if run(f'tar -xzf {server_archive_path} -C {xarch_path}').failed:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
         return False
-    if run(f'rm {server_archive_path}').failed:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-    if run(f'mv {xarch_path}/web_static/* {xarch_path}').failed:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run(f'rm -rf {xarch_path}/web_static').failed:
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
         return False
-    if run('rm -rf /data/web_static/current').failed:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run(f'ln -s {xarch_path} /data/web_static/current').failed:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
         return False
-
-    print('New version deployed!')
     return True
