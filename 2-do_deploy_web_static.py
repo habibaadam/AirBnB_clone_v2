0bf1d3732449104e36f3cd4f8aca3a5a8c62e1e1
habibaadam <habiepalmer@gmail.com>
2023-12-06 19:08:33 +0000

    2. Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 archive to your web servers, using the function do_deploy:"""
 
 from fabric.api import *
-import os 
+import os
 from datetime import datetime
 
 env.user = 'ubuntu'
@@ -14,16 +14,50 @@ def do_pack():
     """ Generates a .tgz archive from the contents of the
     web_static folder
     """
-    now = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(now.year,
-                                                         now.month,
-                                                         now.day,
-                                                         now.hour,
-                                                         now.minute,
-                                                         now.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    now = datetime.today()
+    file = f"versions/web_static_{now.year}{now.month:02d}{now.day:02d}{now.hour:02d}" \
+           f"{now.minute:02d}{now.second:02d}.tgz"
+
+    print(f"Packing web_static to {file}")
+    if not os.path.exists("versions/"):
+        local("mkdir versions/")
+
+    res = local(f"tar -cvzf {file} web_static/")
+    if res.failed:
         return None
-    return file
+    else:
+        archive_size = os.path.getsize(file)
+        print(f"web_static packed: {file} -> {archive_size} Bytes")
+        return file
+
+def do_deploy(archive_p):
+    """Distributes an archive to the web servers"""
+    if not os.path.exists(archive_p):
+        return False
+
+    path = archive_p.split('/')
+    archive_file = path[1]
+    splitted_arhive = archive_file.split('.')
+
+    server_archive_path = f'/tmp/{archive_file}'
+    xarch_path = f'/data/web_static/releases/{splitted_arhive[0]}'
+
+    if put(archive_p, '/tmp/').failed:
+        return False
+    if run(f'mkdir -p {xarch_path}').failed:
+        return False
+    if run(f'tar -xzf {server_archive_path} -C {xarch_path}').failed:
+        return False
+    if run(f'rm {server_archive_path}').failed:
+        return False
+    if run(f'mv {xarch_path}/web_static/* {xarch_path}').failed:
+        return False
+    if run(f'rm -rf {xarch_path}/web_static').failed:
+        return False
+    if run('rm -rf /data/web_static/current').failed:
+        return False
+    if run(f'ln -s {xarch_path} /data/web_static/current').failed:
+        return False
+
+    print('New version deployed!')
+    return True
