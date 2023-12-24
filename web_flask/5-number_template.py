d655490994f2d0df0f88b19d2d2e5a30c0b1417e
habibaadam <habiepalmer@gmail.com>
2023-12-24 17:45:24 +0000

    5. Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -12,7 +12,8 @@ replacing all underscore with a space
 /number/<n>: displays 'n is a number' only if n is an integer
 /number_template/<n>: display a HTML page only if n is an integer
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
